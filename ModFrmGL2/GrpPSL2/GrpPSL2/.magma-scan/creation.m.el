d37f6e0ce91b0c5e2c30ff9426d6861732630b05
assaferan <55987323+assaferan@users.noreply.github.com>
2022-01-03 10:14:48 -0500

    Delete ModFrmGL2/GrpPSL2/GrpPSL2/.magma-scan directory
    This is a binary file, which accidentally found its way here.

--- ModFrmGL2/GrpPSL2/GrpPSL2/.magma-scan/creation.m.el
+++
@@ -1,3 +0,0 @@
-;;; This file was generated automatically.
-
-(setq magma-completion-table (-union magma-completion-table '("get_mod_level" "get_coercion_hom" "init_psl2_group_char" "init_psl2_group" )))
