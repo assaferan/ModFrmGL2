61e0dc56f163b88f99fe60aba642b5b737a72b5c
assaferan <assaferan@gmail.com>
2022-01-20 13:17:50 -0500

    Update shimuraType.m
    adding genus 15 database to shimura type.

--- ModFrmGL2/shimuraType.m
+++ ModFrmGL2/shimuraType.m
@@ -3,7 +3,8 @@ AttachSpec("ModFrmGL2.spec");
 dir := GetCurrentDirectory();
 ChangeDirectory("/Users/eranassaf/Dropbox\ \(Dartmouth\ College\)\
 /Research/ModularSymbols/csg24.dat/");
-load "csg10-lev168.dat";
+// load "csg10-lev168.dat";
+load "csg15-lev240.dat";
 ChangeDirectory(dir);
 
 import "congruence.m" : LoadGroups, qExpansionBasisShimura;
@@ -14,6 +15,7 @@ grps := LoadGroups(L);
 grps_by_genus := LoadGroupsByGenus(L);
 grps_6 := [grps[name] : name in grps_by_genus[6]];
 grps_10 := [grps[name] : name in grps_by_genus[10]];
+grps_15 := [grps[name] : name in grps_by_genus[15]];
 shim6 :=  [ "11A6", "22C6", "31A6", "31B6",
             "58A6", "71A6", "79A6", "121A6" ];
 shim10 :=  [ "9A10", "18E10", "18M10", "26D10", "27B10", "28D10", "36Q10",
@@ -21,14 +23,6 @@ shim10 :=  [ "9A10", "18E10", "18M10", "26D10", "27B10", "28D10", "36Q10",
              , "92A10", "108F10", "127A10" ];
 
 SetDebugOnError(true);
-/*
-ShimuraType6 := [ "11A6", "22C6", "31A6", "31B6",
-		  "58A6", "71A6", "79A6", "121A6" ];
-
-ShimuraType10 := [ "9A10", "18E10", "18M10", "26D10", "27B10", "28D10", "36Q10",
-		   "37A10", "38A10", "46A10", "54A10", "54I10", "81A10", "86A10"
-		   , "92A10", "108F10", "127A10" ];
-*/
 
 procedure computeShimuraModularCurves()
     for name in shim6 do
