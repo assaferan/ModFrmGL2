2c67423ccc361b0d44f98de2fb1cb4dd62005497
assaferan <assaferan@gmail.com>
2022-01-21 19:49:35 -0500

    Updated shimuraType.m
    added groups of shimura type of genus 15

--- ModFrmGL2/shimuraType.m
+++ ModFrmGL2/shimuraType.m
@@ -13,31 +13,38 @@ import "congruence.m" : write_qexps;
 
 grps := LoadGroups(L);
 grps_by_genus := LoadGroupsByGenus(L);
-grps_6 := [grps[name] : name in grps_by_genus[6]];
-grps_10 := [grps[name] : name in grps_by_genus[10]];
-grps_15 := [grps[name] : name in grps_by_genus[15]];
-shim6 :=  [ "11A6", "22C6", "31A6", "31B6",
+
+grps_g := AssociativeArray();
+grps_g[6] := [grps[name] : name in grps_by_genus[6]];
+grps_g[10] := [grps[name] : name in grps_by_genus[10]];
+grps_g[15] := [grps[name] : name in grps_by_genus[15]];
+
+shim := AssociativeArray();
+shim[6] :=  [ "11A6", "22C6", "31A6", "31B6",
             "58A6", "71A6", "79A6", "121A6" ];
-shim10 :=  [ "9A10", "18E10", "18M10", "26D10", "27B10", "28D10", "36Q10",
+shim[10] :=  [ "9A10", "18E10", "18M10", "26D10", "27B10", "28D10", "36Q10",
              "37A10", "38A10", "46A10", "54A10", "54I10", "81A10", "86A10"
              , "92A10", "108F10", "127A10" ];
+shim[15] := [  "35C15", "40W15", "40X15", "43A15", "51A15", "60AC15", "60AD15", 
+	     "67A15", "68D15", "80R15", "80T15", "85A15", "85B15", "102C15", "110A15", 
+	     "136D15", "141A15", "153A15", "155A15", "161A15", "175A15", "179A15",
+	     "193A15" ];
+
 
 // list of best value of M for the different groups
 grps_M := [ <"11A6", 1>, <"14B6", 1>, <"17A6", 1>, <"18A6", 1>, <"18B6", 1>, <"18C6", 1>, <"20C6", 1>, <"21B6", 3>, <"21C6", 1>, <"22A6", 1>, <"22B6", 1>, <"22C6", 1>, <"24D6", 1>, <"24G6", 1>, <"27A6", 1>, <"28D6", 1>, <"28E6", 1>, <"28G6", 1>, <"28I6", 1>, <"30A6", 2>, <"30B6", 1>, <"30C6", 1>, <"30D6", 1>, <"30E6", 3>, <"31A6", 1>, <"31B6", 1>, <"32A6", 1>, <"32B6", 1>, <"35E6", 5>, <"36C6", 1>, <"36D6", 1>, <"36E6", 1>, <"36F6", 1>, <"36J6", 1>, <"36K6", 1>, <"39A6", 1>, <"45A6", 1>, <"45C6", 1>, <"45D6", 1>, <"48A6", 1>, <"48B6", 1>, <"48C6", 1>, <"48D6", 1>, <"48I6", 1>, <"48J6", 1>, <"50E6", 1>, <"52A6", 1>, <"54A6", 1>, <"54B6", 1>, <"56D6", 1>, <"56E6", 1>, <"58A6", 58>, <"60B6", 1>, <"60C6", 1>, <"60E6", 1>, <"60F6", 1>, <"63A6", 7>, <"66B6", 11>, <"66D6", 11>, <"69A6", 23>, <"71A6", 71>, <"72B6", 1>, <"72C6", 1>, <"72D6", 1>, <"78A6", 1>, <"79A6", 79>, <"86A6", 43>, <"87A6", 29>, <"90B6", 1>, <"90C6", 1>, <"100A6", 1>, <"121A6", 121> ];
 
 SetDebugOnError(true);
 
-procedure computeShimuraModularCurves()
-    for name in shim6 do
-	X, fs := qExpansionBasisShimura(name, grps);
-	write_qexps(name cat "_shim", fs, X);
-	print "Group ", name, " - done!";
-    end for;
+procedure computeShimuraModularCurves(shim, grps)
 
-    for name in shim10 do
-	X, fs := qExpansionBasisShimura(name, grps);
-	write_qexps(name cat "_shim", fs, X);
-	print "Group ", name, " - done!";
+    for g in [6,10,15] do
+	for name in shim[g] do
+	    X, fs := qExpansionBasisShimura(name, grps);
+	    write_qexps(name cat "_shim", fs, X);
+	    print "Group ", name, " - done!";
+	end for;
     end for;
+    
 end procedure;
 
